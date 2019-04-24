ad03a3502a94de5ebe4c19681c0e9917f6afccf5
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-04-24 14:10:11 -0600

    Added update and destroy functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: "tasks#show"
+  get '/tasks/:id/edit', to: "tasks#edit"
+  delete '/tasks/:id', to: "tasks#destroy"
+  patch '/tasks/:id', to: "tasks#update"
   post '/tasks', to: 'tasks#create'
 end
